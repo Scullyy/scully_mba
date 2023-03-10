f4bd27bc0a0eb37f50d9b12314dad8de0ad77e0b
Scully <51968381+Scullyy@users.noreply.github.com>
2023-03-10 09:17:04 -0800

    Add new sets

--- config.lua
+++ config.lua
@@ -4,13 +4,23 @@ Config = {
         basketball = {'mba_tribune', 'mba_tarps', 'mba_basketball', 'mba_jumbotron'},
         boxing = {'mba_tribune', 'mba_tarps', 'mba_fighting', 'mba_boxing', 'mba_jumbotron'},
         concert = {'mba_tribune', 'mba_tarps', 'mba_backstage', 'mba_concert', 'mba_jumbotron'},
+        curling = {'mba_tribune', 'mba_chairs', 'mba_curling'},
         derby = {'mba_cover', 'mba_terrain', 'mba_derby', 'mba_ring_of_fire'},
         fameorshame = {'mba_tribune', 'mba_tarps', 'mba_backstage', 'mba_fameorshame', 'mba_jumbotron'},
         fashion = {'mba_tribune', 'mba_tarps', 'mba_backstage', 'mba_fashion', 'mba_jumbotron'},
+        football = {'mba_tribune', 'mba_chairs', 'mba_field', 'mba_soccer'},
+        icehockey = {'mba_tribune', 'mba_chairs', 'mba_field', 'mba_hockey'},
+        gokarta = {'mba_cover', 'mba_gokart_01'},
+        gokartb = {'mba_cover', 'mba_gokart_02'},
+        trackmaniaa = {'mba_trackmania_01', 'mba_cover'},
+        trackmaniab = {'mba_trackmania_02', 'mba_cover'},
+        trackmaniac = {'mba_trackmania_03', 'mba_cover'},
+        trackmaniad = {'mba_trackmania_04', 'mba_cover'},
         mma = {'mba_tribune', 'mba_tarps', 'mba_fighting', 'mba_mma', 'mba_jumbotron'},
         none = {'mba_tribune', 'mba_tarps', 'mba_jumbotron'},
         paintball = {'mba_tribune', 'mba_chairs', 'mba_paintball', 'mba_jumbotron'},
+        rocketleague = {'mba_tribune', 'mba_chairs', 'mba_rocketleague'},
         wrestling = {'mba_tribune', 'mba_tarps', 'mba_fighting', 'mba_wrestling', 'mba_jumbotron'},
-        removals = {'mba_tribune', 'mba_cover', 'mba_tarps', 'mba_chairs', 'mba_basketball', 'mba_derby', 'mba_paintball', 'mba_fighting', 'mba_wrestling', 'mba_mma', 'mba_boxing', 'mba_backstage', 'mba_concert', 'mba_fashion', 'mba_fameorshame', 'mba_ring_of_fire', 'mba_jumbotron', 'mba_terrain'}
+        removals = {'mba_trackmania_04', 'mba_trackmania_03', 'mba_trackmania_02', 'mba_trackmania_01', 'mba_gokart_02', 'mba_gokart_01', 'mba_hockey', 'mba_field', 'mba_soccer', 'mba_rocketleague', 'mba_curling', 'mba_tribune', 'mba_cover', 'mba_tarps', 'mba_chairs', 'mba_basketball', 'mba_derby', 'mba_paintball', 'mba_fighting', 'mba_wrestling', 'mba_mma', 'mba_boxing', 'mba_backstage', 'mba_concert', 'mba_fashion', 'mba_fameorshame', 'mba_ring_of_fire', 'mba_jumbotron', 'mba_terrain'}
     }
 }
